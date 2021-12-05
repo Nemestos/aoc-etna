c93cd16811fec99bf49df48d39e84af5eef5c52b
turpin_l <turpin_l@etna-alternance.net>
2021-12-05 15:52:25 +0100

    day 05 part 02

--- day05/day05.py
+++ day05/day05.py
@@ -31,13 +31,17 @@ class Day05(Day):
                 total += j >= 2
         return total
 
-    def line_cover(self, pos):
+    def line_cover(self, pos, diag=False):
         a, b = pos[0], pos[1]
         if a.x == b.x:
-
             return [Point(a.x, i) for i in range(min(a.y, b.y), max(a.y, b.y) + 1)]
         elif a.y == b.y:
             return [Point(i, a.y) for i in range(min(a.x, b.x), max(a.x, b.x) + 1)]
+        elif diag:
+            x = (-1 if a.x > b.x else 1)
+            y = (-1 if a.y > b.y else 1)
+            # cas diagonale
+            return [Point(i, j) for i, j in zip(range(a.x, b.x + 1 * x, x), range(a.y, b.y + 1 * y, y))]
         else:
             return []
 
@@ -58,5 +62,15 @@ class Day05(Day):
 
     @timeit
     def part2(self) -> tuple[int, int]:
-        return 0
-        # input = self.input_list(int, '\n', True, ",")
+        input = self.input_list(lambda a: [Point(*list(map(int, i.split(",")))) for i in a.split(" -> ")])
+        width, height = self.calculate_size(input)
+        grid = []
+        for i in range(height + 1):
+            grid.append([0] * (width + 1))
+        for i in input:
+            cover = self.line_cover(i, True)
+
+            if cover != []:
+                for p in cover:
+                    grid[p.y][p.x] += 1
+        return self.inter_lines(grid)
